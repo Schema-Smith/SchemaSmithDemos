d804fdaebd717d39a422bd2b9668c39eefe1935f
Agent Smith <agentsmith7171@gmail.com>
2025-04-13 17:03:02 -0400

    Converted the product and template to free and added the seed data

---
+++ Northwind/Templates/Northwind/MigrationScripts/After/SeedData/1 - Region.sql
@@ -0,0 +1,4 @@
+Insert Region(RegionID, RegionDescription) Values (1,'Eastern')
+Insert Region(RegionID, RegionDescription) Values (2,'Western')
+Insert Region(RegionID, RegionDescription) Values (3,'Northern')
+Insert Region(RegionID, RegionDescription) Values (4,'Southern')
