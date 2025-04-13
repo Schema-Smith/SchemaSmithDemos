4b3203cdb61d6d12df6ab2823a8d4c89cd3484c2
Agent Smith <agentsmith7171@gmail.com>
2025-04-13 08:47:17 -0700

    Initial commit

---
+++ Northwind/Templates/Northwind/Views/dbo.Customer and Suppliers by City.sql
@@ -0,0 +1,9 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE OR ALTER view [dbo].[Customer and Suppliers by City] AS
+
+SELECT City, CompanyName, ContactName, 'Customers' AS Relationship 
+FROM Customers
+UNION SELECT City, CompanyName, ContactName, 'Suppliers'
+FROM Suppliers
---
+++ Northwind/Templates/Northwind/Views/dbo.Products by Category.sql
@@ -0,0 +1,8 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE OR ALTER view [dbo].[Products by Category] AS
+
+SELECT Categories.CategoryName, Products.ProductName, Products.QuantityPerUnit, Products.UnitsInStock, Products.Discontinued
+FROM Categories INNER JOIN Products ON Categories.CategoryID = Products.CategoryID
+WHERE Products.Discontinued <> 1
