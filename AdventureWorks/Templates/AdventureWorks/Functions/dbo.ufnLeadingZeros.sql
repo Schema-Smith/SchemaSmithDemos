c86be847108d570ee0b464fed9d845cd17a09305
Agent Smith <agentsmith7171@gmail.com>
2025-04-14 10:31:10 -0400

    Add Adventure works schema

---
+++ AdventureWorks/Templates/AdventureWorks/Functions/dbo.ufnLeadingZeros.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE OR ALTER FUNCTION [dbo].[ufnLeadingZeros](
+    @Value int
+) 
+RETURNS varchar(8) 
+WITH SCHEMABINDING 
+AS 
+
+BEGIN
+    DECLARE @ReturnValue varchar(8);
+
+    SET @ReturnValue = CONVERT(varchar(8), @Value);
+    SET @ReturnValue = REPLICATE('0', 8 - DATALENGTH(@ReturnValue)) + @ReturnValue;
+
+    RETURN (@ReturnValue);
+END
