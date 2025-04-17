ddac04c065d09ffda87d06ad48bffa5b86f4b25a
Agent Smith <agentsmith7171@gmail.com>
2025-04-17 11:55:26 -0400

    Renamed the server, separated seed data from sample data

--- Northwind/Templates/Northwind/MigrationScripts/After/SeedData/1 - Region.sql
+++
@@ -1,4 +0,0 @@
-INSERT Region(RegionID, RegionDescription) VALUES(1,'Eastern')
-INSERT Region(RegionID, RegionDescription) VALUES(2,'Western')
-INSERT Region(RegionID, RegionDescription) VALUES(3,'Northern')
-INSERT Region(RegionID, RegionDescription) VALUES(4,'Southern')
